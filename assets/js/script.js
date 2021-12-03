9cbee4c28d1d34fa65e3769eaafcdc3d0dbb6be0
Niraj Limbu <nlimbu@yahoo.com>
2021-12-03 12:22:17 -0500

    add moment and jquery files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,4 @@
-console.log('Hello world');
+// Display today's day and date
+var todayEl = document.querySelector('#currentDay');
+todayDate = moment().format('dddd, MMM Do YYYY');
+todayEl.textContent = todayDate;
